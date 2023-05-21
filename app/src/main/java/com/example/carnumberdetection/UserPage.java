d9cfed523230bae05dc58d9bc5186a81d1e54786
admin <ahmed.g@live.com>
2023-05-21 18:09:38 +0300

    `new`

--- app/src/main/java/com/example/carnumberdetection/UserPage.java
+++ app/src/main/java/com/example/carnumberdetection/UserPage.java
@@ -1,15 +1,21 @@
 package com.example.carnumberdetection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
 public class UserPage extends AppCompatActivity {
     String id;
     Button scan,reports,profile,contact;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_CAMERA_PERMISSION = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +31,7 @@ public class UserPage extends AppCompatActivity {
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserPage.this, PlateDetectionActivity.class);
+               Intent intent = new Intent(UserPage.this, PlateDetectionActivity.class);
                 intent.putExtra("id", id);
                 startActivity(intent);
             }
@@ -42,7 +48,6 @@ public class UserPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-
             }
         });
 
@@ -54,5 +59,15 @@ public class UserPage extends AppCompatActivity {
             }
         });
 
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        System.out.println("good2");
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            System.out.println("good3");
+
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
